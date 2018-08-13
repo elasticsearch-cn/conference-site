1e1ed7e133ef5b3bec9101d48cd0d83d2eef304f
shiyang <liaoshiyang@foschool.cn>
2018-08-13 18:42:12 +0800

    update quotes

--- 2018/js/quotes.js
+++ 2018/js/quotes.js
@@ -6,11 +6,22 @@ function getQuote() {
         url : "/2018/js/quotes.json",
         datatype: "json",
         success : function(data) {
-            var index = Math.floor((Math.random() * data.length));
+            var index = random(0, data.length - 1);
+            console.log(index);
             var item = data[index];
             $(".quotes-content").text(item.content);
             $(".quotes-company").text(item.company + item.title);
             $(".quotes-name").text(item.name);
         }
     });
+}
+
+/**
+ * 指定区间获取随机整数
+ * @param min
+ * @param max
+ * @returns {number}
+ */
+function random(min,max) {
+    return parseInt(Math.random()*(max-min+1)+min,10);
 }
