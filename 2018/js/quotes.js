9c6121522eea9213c931edf14b3382e49025885e
medcl <m@medcl.net>
2018-08-14 14:57:06 +0800

    update conf page

--- 2018/js/quotes.js
+++ 2018/js/quotes.js
@@ -1,19 +1,34 @@
 //大会推荐语
-getQuote();
 function getQuote() {
-    $.ajax({
-        type:'GET',
-        url : "/2018/js/quotes.json",
-        datatype: "json",
-        success : function(data) {
-            var index = random(0, data.length - 1);
+
+data =[
+  {
+    "name":"黄思宇",
+    "company":"阿里巴巴",
+    "title":"技术专家",
+    "content":"参加过，值得推荐！"
+  },
+  {
+    "name":"黄琛",
+    "company":"OneAPM",
+    "title":"首席架构师",
+    "content":"Elastic技术栈非常适合半结构化数据的收集、存储和分析，用好能够极大提高生产效率。参加开发者大会能够开阔眼界，学习广大同行如何更好让Elastic落地，非常有价值！"
+  },
+  {
+    "name":"白凡",
+    "company":"尚德机构",
+    "title":"工程师",
+    "content":"这是一个ES交流很棒的平台，除了能学到更多优秀开发者的分享，也能拿到很多不错的周边。"
+  }
+]
+
+var index = random(0, data.length - 1);
             console.log(index);
             var item = data[index];
             $(".quotes-content").text(item.content);
             $(".quotes-company").text(item.company + item.title);
             $(".quotes-name").text(item.name);
-        }
-    });
+
 }
 
 /**
@@ -25,3 +40,5 @@ function getQuote() {
 function random(min,max) {
     return parseInt(Math.random()*(max-min+1)+min,10);
 }
+
+getQuote();
